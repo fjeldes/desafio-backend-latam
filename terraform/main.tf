1a9f7896cac68327662c73a2227e3d1e7ec4c2dd
Felipe Jeldes Ramirez <felipejeldesramirez@MacBook-Pro-de-Felipe.local>
2026-05-26 19:00:52 -0400

    Add artifactregistry.writer role to Cloud Build SA

--- terraform/main.tf
+++ terraform/main.tf
@@ -195,3 +195,9 @@ resource "google_project_iam_member" "cloudbuild_sa_user" {
   role    = "roles/iam.serviceAccountUser"
   member  = "serviceAccount:${data.google_project.current.number}@cloudbuild.gserviceaccount.com"
 }
+
+resource "google_project_iam_member" "cloudbuild_artifact_writer" {
+  project = var.project_id
+  role    = "roles/artifactregistry.writer"
+  member  = "serviceAccount:${data.google_project.current.number}@cloudbuild.gserviceaccount.com"
+}
